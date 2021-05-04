fe6b2e62b4ba939745e365bd3f456ca4ccd06094
nhatnt <nhatnt@vng.com.vn>
2021-05-04 17:05:06 +0700

    chore(linkedlist): playgrounds are self-contained -> sources/* referencing each other other in Playground ref: https://stackoverflow.com/questions/33880727/sources-referencing-each-other-in-xcode-playgrounds

--- LinkedList/LinkedList.playground/Sources/LinkedList.swift
+++ LinkedList/LinkedList.playground/Sources/LinkedList.swift
@@ -1,6 +1,28 @@
 //  Created by nhatnt on 23/04/2021.
 import Foundation
 
+//public class Node<Value> {
+//    
+//    public var value: Value
+//    public var next: Node?
+//    
+//    public init (value: Value, next: Node? = nil) {
+//        self.value = value
+//        self.next = next
+//    }
+//}
+//
+//extension Node: CustomStringConvertible {
+//    
+//    public var description: String {
+//        guard let next = next else {
+//            return "\(value)"
+//        }
+//        
+//        return "\(value) -> " + String(describing: next) + " "
+//    }
+//}
+
 public struct LinkedList<Value> {
     public var head: Node<Value>?
     public var tail: Node<Value>?

a27a59fb79a98775831450050b2757ac61d76100
Nguyễn Tài Nhật <nguyentainhat2110@gmail.com>
2022-05-14 00:40:39 +0700

    chore(linked-list): add example of using collection list

--- LinkedList.swift
+++ LinkedList.swift
@@ -220,3 +220,18 @@ extension LinkedList: Collection {
         return Index(node: i.node?.next)
     }
 }
+
+//Example of using Collection
+
+var collectionList = LinkedList<Int>()
+for i in 0...9 {
+    collectionList.append(i)
+}
+
+print("List: \(collectionList)")
+print("First element: \(collectionList[collectionList.startIndex])")
+print("Array containing first 3 elements: \(Array(collectionList.prefix(3)))")
+print("Array containing last 3 elements: \(Array(collectionList.suffix(3)))")
+
+let sum = list.reduce(0, +)
+print("Sum of all values: \(sum)")

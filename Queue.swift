461b44ec0a59129062afe128724598aadbfaafe5
Nguyễn Tài Nhật <nguyentainhat2110@gmail.com>
2022-05-14 17:13:11 +0700

    feat(queue): implement queue by two stacks

--- Queue.swift
+++ Queue.swift
@@ -27,7 +27,7 @@ public struct QueueArray<T>: Queue {
     }
     
     public mutating func dequeue() -> T? {
-        return array.popLast()
+        return isEmpty ? nil : array.removeFirst()
     }
 }
 
@@ -51,3 +51,54 @@ print("Dequeue: \(queue.dequeue() ?? "")")
 print("After dequeue: \(queue)")
 
 print("Peek: \(queue.peek ?? "")")
+
+//Double Stack Implementation
+
+public struct QueueStack<T>: Queue {
+    private var leftStack = [T]()
+    private var rightStack = [T]()
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        return leftStack.isEmpty && rightStack.isEmpty
+    }
+    
+    public var peek: T? {
+        return !leftStack.isEmpty ? leftStack.last : rightStack.first
+    }
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        rightStack.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        if leftStack.isEmpty {
+            leftStack = rightStack.reversed()
+            rightStack.removeAll()
+        }
+        
+        return leftStack.popLast()
+    }
+}
+
+
+extension QueueStack: CustomStringConvertible {
+    public var description: String {
+        let printList = leftStack.reversed() + rightStack
+        return printList.description
+    }
+}
+
+var queueStack = QueueStack<String>()
+_ = queueStack.enqueue("Nguyen")
+_ = queueStack.enqueue("Tai")
+_ = queueStack.enqueue("Nhat")
+
+print("\(queueStack)")
+
+print("Dequeue: \(queueStack.dequeue() ?? "")")
+print("After dequeue: \(queueStack)")
+
+print("Peek: \(queueStack.peek ?? "")")
